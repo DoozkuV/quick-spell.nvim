6894c6a39f3744d5ab44cf0d0cde54d7d42ccad7
George Padron <georgenpadron@gmail.com>
2024-10-25 21:55:04 -0500

    fix: empty usercommand being deleted

--- plugin/quick-spell.lua
+++ plugin/quick-spell.lua
@@ -4,17 +4,19 @@ if vim.g.loaded_quick_spell then
 end
 vim.g.loaded_quick_spell = 1
 
+local cmd_name = "QuickSpell"
+
 vim.api.nvim_create_augroup("quick-spell", { clear = true })
 vim.api.nvim_create_autocmd("OptionSet", {
     group = "quick-spell",
     pattern = "spell",
     callback = function()
         if vim.wo.spell then
-            vim.api.nvim_buf_create_user_command(0, "QuickSpell",
+            vim.api.nvim_buf_create_user_command(0, cmd_name,
                 function() require("quick-spell").correct_word() end,
                 { nargs = 0 })
-        else
-            vim.api.nvim_buf_del_user_command(0, "QuickSpell")
+        elseif vim.api.nvim_buf_get_commands(0, {})[cmd_name] ~= nil then
+            vim.api.nvim_buf_del_user_command(0, cmd_name)
         end
     end
 })
